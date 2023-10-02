53422a5d4f83a45a7e7e7ac9281bbe24393fee9f
Coow <eirik_skarding@hotmail.no>
2023-10-03 01:36:17 +0200

    Add Quick buttons for timer, Live Output & Better User Action feedback

--- src/dashboard/CountdownTimer.tsx
+++ src/dashboard/CountdownTimer.tsx
@@ -1,7 +1,11 @@
 import React, { useState, useEffect } from 'react'
 import { useReplicant } from 'use-nodecg'
+import { differenceInSeconds } from 'date-fns'
+import { Tooltip as ReactTooltip } from "react-tooltip";
+
 
 export function CountdownTimer() {
+  let interval: any
   const [countdownTimerActive, setCountdownTimerActive] = useReplicant<boolean>(
     'countdownTimerActive',
     false
@@ -10,7 +14,18 @@ export function CountdownTimer() {
     'countdownTimerEnd',
     ''
   )
+  const [timerEndText, setTimerEndText] = useReplicant<string>(
+    'timerEndText',
+    ''
+  )
   const [countdownTime, setCountdownTime] = useState('')
+
+  const [errorMessage, setErrorMessage] = useState(<>&nbsp;</>)
+
+  const [timer, setTimer] = useReplicant<string>(
+    'timer',
+    ''
+  )
 
   const validateAndSetCountdownTimer = () => {
     // If countdown is currently active and the user just hit stop, ensure this happens regardless of if we have a valid time
@@ -18,14 +33,23 @@ export function CountdownTimer() {
       setCountdownTimerActive(!countdownTimerActive)
     }
 
-    const timeParts = countdownTime.split(':')
-    if (timeParts?.length !== 2) return
+    const timeParts = countdownTime.split(/[\sm:;.,]/)
+    if (timeParts?.length !== 2) {
+      setErrorMessageText("Missing Seperator (: . , ;)")
+      return
+    }
 
     const minutes = parseInt(timeParts[0])
     const seconds = parseInt(timeParts[1])
 
-    if (minutes > 5) return
-    if (seconds > 59) return
+    if (minutes > 59 || isNaN(minutes)) {
+      setErrorMessageText("59 Minutes is long enough...")
+      return
+    }
+    if (seconds > 59 || isNaN(seconds)) {
+      setErrorMessageText("Maybe make this a minute instead?")
+      return
+    }
 
     const timerEndDate = new Date()
     timerEndDate.setMinutes(timerEndDate.getMinutes() + minutes)
@@ -38,25 +62,64 @@ export function CountdownTimer() {
     setCountdownTimerEnd(timerEndDate.toString())
   }
 
+  async function setErrorMessageText(msg: string) {
+    setErrorMessage(<>{msg}</>)
+    await new Promise(resolve => setTimeout(resolve, 4000));
+    setErrorMessage(<>&nbsp;</>)
+  }
+
   return (
-    <>
-      <div>
-        <div className='flex flex-row'>
+    <div className='py-4'>
+      <div className='flex flex-col justify-center items-center'>
+        <h1 className='text-1xl'>Live Output</h1>
+        <h1 className='text-5xl'>{countdownTimerActive ? timer : <span className='italic text-3xl'>Timer Hidden</span>}</h1>
+        <h1 className='text-3xl text-red-600'>{errorMessage}</h1>
+      </div>
+      <div className='flex flex-row justify-evenly'>
+        <div className='w-5/12 px-8'>
           <input
             type='text'
             placeholder='5:00'
-            name='aoe2cmDraft'
-            className='w-3/5'
+            name='timerInputTextField'
+            className='w-1/5 text-black timerInputTextField'
             value={countdownTime}
             onChange={(e) => setCountdownTime(e.currentTarget.value)}
           />
-          <div className='px-16 w-2/5'>
-            <button onClick={validateAndSetCountdownTimer}>
-              {!countdownTimerActive ? 'Start Timer' : 'Stop Timer'}
-            </button>
-          </div>
+          <ReactTooltip
+            anchorSelect=".timerInputTextField"
+            id="tooltip1"
+            place="bottom"
+            //@ts-ignore
+            content={<div className='text-xl'>Input Field for Timer. <br /> Valid formats are: | 1:05 | 1m05 | 1.05 | 1,05 | 1 05 | 1;05<br />All of these will result in a timer of 1 min 5 sec. 05 can be replaced with 5</div>}
+          />
+        </div>
+        <button className={`${!countdownTimerActive ? 'bg-green-500 hover:bg-green-700' : 'bg-red-500 hover:bg-red-700'} my-2 px-4 text-white w-5/12`} onClick={validateAndSetCountdownTimer}>
+          {!countdownTimerActive ? 'Start Timer' : 'Stop Timer'}
+        </button>
+      </div>
+      <hr />
+      <div className='flex flex-col py-4 items-center'>
+        <h1 className='text-2xl pb-2'>Time Presets</h1>
+        <div className='flex flex-row justify-evenly w-full'>
+          <button className='bg-blue-500 hover:bg-blue-700 mx-4 py-2' onClick={() => setCountdownTime('1:00')}>1 min</button>
+          <button className='bg-blue-500 hover:bg-blue-700 mx-4 py-2' onClick={() => setCountdownTime('2:00')}>2 min</button>
+          <button className='bg-blue-500 hover:bg-blue-700 mx-4 py-2' onClick={() => setCountdownTime('3:00')}>3 min</button>
+          <button className='bg-blue-500 hover:bg-blue-700 mx-4 py-2' onClick={() => setCountdownTime('4:00')}>4 min</button>
+          <button className='bg-blue-500 hover:bg-blue-700 mx-4 py-2' onClick={() => setCountdownTime('5:00')}>5 min</button>
         </div>
       </div>
-    </>
+      <hr />
+      <div className='p-4 flex flex-col items-center justify-center'>
+        <h1 className='text-2xl'>Timer End Text</h1>
+        <input
+          type='text'
+          placeholder='Timer Done!'
+          name='timerEndText'
+          className=' text-black'
+          value={timerEndText}
+          onChange={(e) => setTimerEndText(e.currentTarget.value)}
+        />
+      </div>
+    </div>
   )
 }
--- src/graphics/Index.tsx
+++ src/graphics/Index.tsx
@@ -12,6 +12,16 @@ export function Index() {
     'countdownTimerEnd',
     ''
   )
+  const [timerEndText, setTimerEndText] = useReplicant<string>(
+    'timerEndText',
+    ''
+  )
+
+  const [timer, setTimer] = useReplicant<string>(
+    'timer',
+    ''
+  )
+
   const [secondsRemaining, setSecondsRemaining] = useState(0)
   console.log('render')
 
@@ -41,17 +51,22 @@ export function Index() {
     return <></>
   }
 
+  
   if (secondsRemaining <= 0) {
     interval = clearInterval(interval)
+    console.log("timer Finsihed")
+    setTimer(timerEndText ? timerEndText : '0:00')
     return (
-      <div className={'countdownTimerContainer'}>Starting Soon™</div>
-    )
-  }
-
-  const minutes = Math.floor(secondsRemaining / 60)
-  const seconds = secondsRemaining - minutes * 60
-
-  const formattedTime = `${minutes}:${seconds < 10 ? '0' : ''}${seconds}`
-
+      <div className={'countdownTimerContainer'}>{timerEndText ? timerEndText : '0:00'}</div>
+      )
+    }
+    
+    const minutes = Math.floor(secondsRemaining / 60)
+    const seconds = secondsRemaining - minutes * 60
+  
+    const formattedTime = `${minutes}:${seconds < 10 ? '0' : ''}${seconds}`
+  
+    setTimer(formattedTime)
+    
   return <div className={'countdownTimerContainer'}>{formattedTime}</div>
 }
